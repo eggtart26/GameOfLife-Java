ae2e15a9ed2598323255a0de6fc493c55f1c002b
eggtart26 <justin.guan26@gmail.com>
2020-08-03 21:23:40 -0700

    setup panel&button

--- src/gameoflife/java/GameOfLifeJava.java
+++ src/gameoflife/java/GameOfLifeJava.java
@@ -16,6 +16,9 @@ public class GameOfLifeJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        GameOfLife_JFrame life = new GameOfLife_JFrame();
+        life.setLocationRelativeTo(null);
+        life.show();
     }
     
 }
