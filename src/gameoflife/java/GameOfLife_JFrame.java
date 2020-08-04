e49914e64cc3bbb53482db104638ca8042b1aa4a
eggtart26 <justin.guan26@gmail.com>
2020-08-04 00:56:39 -0700

    implemented repain

--- src/gameoflife/java/GameOfLife_JFrame.java
+++ src/gameoflife/java/GameOfLife_JFrame.java
@@ -4,8 +4,12 @@
  * and open the template in the editor.
  */
 package gameoflife.java;
+
 import java.util.Timer;
 import java.util.TimerTask;
+import java.awt.Image;
+import java.awt.Graphics;
+import java.awt.Color;
 
 /**
  *
@@ -13,14 +17,35 @@ import java.util.TimerTask;
  */
 public class GameOfLife_JFrame extends javax.swing.JFrame {
     
-    
-    boolean[][] currentMove = new boolean[100][200], nextMove = new boolean[100][200] ;
+    final int wid = 200, hei = 100;
+    boolean[][] currentMove = new boolean[hei][wid], nextMove = new boolean[hei][wid] ;
     boolean play;
-    
+    Image offScrImg;
+    Graphics offScrGraph;
     
     public GameOfLife_JFrame() {
         initComponents();
+        offScrImg = createImage(jPanel1.getWidth(), jPanel1.getHeight());
+        offScrGraph = offScrImg.getGraphics();
+        repain();
     }
+    
+    private void repain(){
+        offScrGraph.setColor(jPanel1.getBackground());
+        offScrGraph.fillRect(0, 0, jPanel1.getWidth(), jPanel1.getHeight());
+        offScrGraph.setColor(Color.BLACK);
+        for(int i = 1; i < hei; i += 1) {
+            int y = i * jPanel1.getHeight() / hei;
+            offScrGraph.drawLine(0, y, jPanel1.getWidth(), y);
+        }
+        for(int j = 1; j < wid; j += 1) {
+            int x = j * jPanel1.getHeight() / wid;
+            offScrGraph.drawLine(x, 0, x, jPanel1.getHeight());   
+        }
+        
+        jPanel1.getGraphics().drawImage(offScrImg, 0, 0, jPanel1);
+    }
+    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -31,15 +56,27 @@ public class GameOfLife_JFrame extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(102, 102, 102));
+        jPanel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jPanel1MouseClicked(evt);
+            }
+        });
+        jPanel1.addComponentListener(new java.awt.event.ComponentAdapter() {
+            public void componentResized(java.awt.event.ComponentEvent evt) {
+                jPanel1ComponentResized(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 764, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 333, Short.MAX_VALUE)
+            .addGap(0, 403, Short.MAX_VALUE)
         );
 
         jButton1.setText("Play");
@@ -51,29 +88,41 @@ public class GameOfLife_JFrame extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(15, 15, 15)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(layout.createSequentialGroup()
                         .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 487, Short.MAX_VALUE)
                         .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(21, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(16, 16, 16)
+                .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addGap(18, 18, 18)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton2))
+                    .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 29, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jPanel1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jPanel1MouseClicked
+        // TODO add your handling code here:
+        repain();
+    }//GEN-LAST:event_jPanel1MouseClicked
+
+    private void jPanel1ComponentResized(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_jPanel1ComponentResized
+        // TODO add your handling code here:
+        offScrImg = createImage(jPanel1.getWidth(), jPanel1.getHeight());
+        offScrGraph = offScrImg.getGraphics();
+        repain();
+    }//GEN-LAST:event_jPanel1ComponentResized
 
     
     
